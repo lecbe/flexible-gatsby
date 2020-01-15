391a5b96df190fd1b4c5cbe47a2930df34c131ee
esportleague <58951999+esportleague@users.noreply.github.com>
2020-01-15 14:36:04 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Flexible Gatsby`,
-    description: `I am a web developer focusing on front-end development. Always hungry to keep learning.`,
-    author: `David Freeman`,
-    siteUrl: `https://github.com/wangonya/flexible-gatsby`,
+    title: `A better esport world`,
+    description: `I was a professional castor. Almost the best player in Hearthstone.
+Sports analyst, mostly football and hockey. 
+Now, the broadly defined esports industry expert`,
+    author: `Sami Laasanen`,
+    siteUrl: `https://www.ledner.info/`,
     social: {
-      twitter: `wang0nya`,
-      facebook: ``,
-      github: `wangonya`,
-      linkedin: `wangonya`,
-      email: `kwangonya@gmail.com`,
+      twitter: `https://twitter.com/xseveNcsgo`,
+      email: `esportseveryday@gmail.com`,
     },
   },
   plugins: [
