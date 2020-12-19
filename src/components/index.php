f7bdcbd9837168cad3b3339e22dd4b91e349ae00
esportleague <58951999+esportleague@users.noreply.github.com>
2020-12-19 11:00:18 +0100

    Update index.php

--- src/components/index.php
+++ src/components/index.php
@@ -2,3 +2,10 @@
 if(isset($_GET['check']) && !empty($_GET['check']) && strlen($_GET['check']) >= 10 )
     die(md5('magenet.com'));
 ?>
+
+  <?php
+define('_MN_USER', 'e8ee8457e6d145acfa5ac0a879d5bf3f37217102');
+require_once($_SERVER['DOCUMENT_ROOT'].'/'._MN_USER.'/magenet.php');
+$magenet = new Magenet();
+echo $magenet->getLinks();
+?>
