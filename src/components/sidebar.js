4b85326c14008d3bdf3f7f7e8088ef1aa4528997
esportleague <58951999+esportleague@users.noreply.github.com>
2020-12-19 10:59:44 +0100

    Update sidebar.js

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -10,12 +10,7 @@ const Sidebar = ({ siteMetadata }) => (
         <div className="about">
   
   
-  <?php
-define('_MN_USER', 'e8ee8457e6d145acfa5ac0a879d5bf3f37217102');
-require_once($_SERVER['DOCUMENT_ROOT'].'/'._MN_USER.'/magenet.php');
-$magenet = new Magenet();
-echo $magenet->getLinks();
-?>
+
   
   
           <div className="cover-author-image">
